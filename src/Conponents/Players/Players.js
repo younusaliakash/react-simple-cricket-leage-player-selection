2bd14f2604294eb66e47268712bc6a90902d4d25
Younus Ali Akash <younusaliakash8@gmail.com>
2021-03-01 11:46:37 +0600

    fully done

--- src/Conponents/Players/Players.js
+++ src/Conponents/Players/Players.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Players.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckDouble, faCoffee, faMedal } from '@fortawesome/free-solid-svg-icons'
+import { faCheckDouble, faMedal } from '@fortawesome/free-solid-svg-icons'
 
 
 const Players = (props) => {
--- src/Conponents/SelectedPlayerInfo/SelectedPlayerInfo.js
+++ src/Conponents/SelectedPlayerInfo/SelectedPlayerInfo.js
@@ -4,7 +4,7 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons'
 import './SelectedPlayerInfo.css'
 
 const SelectedPlayerInfo = (props) => {
-    const {name,role,picture} = props.playerInfo
+    const {name,role,picture,salary} = props.playerInfo
     return (
         <div>
             <div className="col player-title-card">
@@ -18,7 +18,10 @@ const SelectedPlayerInfo = (props) => {
                 </div>
                 <div className="player-title">
                     <h6 className="player-name">{name}</h6>
-                    <p>{role}</p>
+                    <div className="d-flex justify-content-between align-items-center px-2">
+                    <p>{role} </p>
+                    <p>${salary}</p>
+                    </div>
                 </div>
             </div>
         </div>
